f9036bbb1e8920c2828b02242356e7c3d5d2a842
Liz P <parisielizabeth@gmail.com>
2020-10-10 22:27:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ function BoardMember(name, homeState, training) {
   this.training = training;
 }
 
-BoardMember.prototype
+BoardMember.prototype.veto = function() {
+  return 'No, I must disagree';
+}
+
+BoardMember.prototype.approve = function() {
+  return 'You can do that!';
+}
+
+BoardMember.prototype.doCharity = function() {
+  return 'I like to help people.';
+}
